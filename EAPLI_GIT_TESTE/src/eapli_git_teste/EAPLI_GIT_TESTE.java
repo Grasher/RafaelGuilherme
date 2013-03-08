254e67de673adf8558abb82c80fa89becda814ee
i110617 <i110617@PCI086.dei.isep.ipp.pt>
2013-03-08 15:07:14 +0000

    Alterar o Main File

--- EAPLI_GIT_TESTE/src/eapli_git_teste/EAPLI_GIT_TESTE.java
+++ EAPLI_GIT_TESTE/src/eapli_git_teste/EAPLI_GIT_TESTE.java
@@ -14,6 +14,14 @@ public class EAPLI_GIT_TESTE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        escrever_Gambit_Gaming();
         System.out.println("TESTE");
+        
+        
+    }
+    
+    public static void escrever_Gambit_Gaming()
+    {
+        System.out.printf("Gambit Gaming!\n");
     }
 }
